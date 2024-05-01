62b70a1491275782cfd4a7469a5efc69ec3bfcf2
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2024-05-02 00:44:20 +0530

    common: Add support for crow chipset
    Change-Id: I1738de8c80a46b535f5cc1558d5271df733919f9
    Signed-off-by: Venkat Yadlapati <venkat.yadlapati@hotmail.com>

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -10,4 +10,4 @@ UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
 UM_4_19_FAMILY := kona lito bengal
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
-UM_5_15_FAMILY := kalama
+UM_5_15_FAMILY := kalama crow
